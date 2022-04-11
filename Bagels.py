2b728ebd01e3c612183175a2eed61597f0947e63
ivan555-lab <ivanchapay555@gmail.com>
2022-04-11 09:13:06 +0300

    '1147'

--- Bagels.py
+++ Bagels.py
@@ -1,1 +1,49 @@
+import random
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+    print('''Bagels, a deductive logic game.
+    I am thinking of a {}-digit number with no repeated digits.
+    Try to guess what it is. Here are some clues:
+    When I say: That means:
+     Pico One digit is correct but in the wrong position.
+     Fermi One digit is correct and in the right position.
+    Bagels No digit is correct.
+    For example, if the secret number was 248 and your guess was 843, the
+    clues would be Fermi Pico.''')
+
+        while True:
+            secretNum = getSecretNum()
+            print('I have thought up a number.')
+            print(' You have {} guesses to get it.'.format(MAX_GUESSES))
+
+            numGuesses = 1
+            while numGuesses <= MAX_GUESSES:
+                guess = ''
+                while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                    print('Guess #{}: '.format(numGuesses))
+                    guess = input('> ')
+
+                clues = getClues(guess, secretNum)
+                print(clues)
+                numGuesses += 1
+
+                if guess == secretNum:
+                    break
+                if numGuesses > MAX_GUESSES:
+                    print('You ran out of guesses')
+                    print('The answer was {}.'.format(secretNum))
+
+            print('Do you want to play again? (yes or no)')
+            if not input('> ').lower().startwith('y'):
+                break
+        print('Thanks for playing!')
+
+def getSecretNum():
+    
+
+
+
 
