58bed63c73d0b34b6ca5020018e982b2fd2976d5
ivan555-lab <ivanchapay555@gmail.com>
2022-04-12 09:39:08 +0300

    '1243'

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,7 +6,9 @@ class Cat:
         self.name = name
 
 my_Cat = Cat('W', 'G', 'M')
+my_Cat.name = 'Gregory Andelov'
+my_Cat.wool_color = 'B'
 print("Cl name - ", my_Cat.Name_Class)
-print("Here is our Cat:")
+print("Here is our Cat:", my_Cat.name)
 print("W col- ", my_Cat.wool_color)
 print('Y-', my_Cat.eyes_color)
