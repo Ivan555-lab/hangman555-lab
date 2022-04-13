efb65332c9e31bdd92706b16598f0c3c75ae7ce7
ivan555-lab <ivanchapay555@gmail.com>
2022-04-13 13:55:36 +0300

    Ex:hello

---
+++ hello/firstapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+
+def index(request):
+    #return render(request, "index.html")
+    #data = {"header": "Передача параметров в шаблон Django",
+    #        "message":
+    #        "Загружен шаблон templates/index_app1.html"}
+    #return render(request, "firstapp/index_app1.html", context=data)
+    header = "Персональные данные"
+    langs = ["Английский", "Немецкий", "Испанский"]
+    user = {"name": "Максим,", "age": 30}
+    addr = ("Виноградная", 23, 45)
+    data = {"header": header, "langs": langs, "user": user, "address": addr}
+    return render(request, "index.html", context=data)
+
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
+
+def products(request, productid):
+    category = request.GET.get("cat", "")
+    output = "<h2>Продукт № {0} Категория: {1}</h2>".format(productid, category)
+    return HttpResponse(output)
+
+def users(request):
+    id = request.GET.get("id", 1)
+    name = request.GET.get("name", "Максим")
+    output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3>".format(id, name)
+    return HttpResponse(output)
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
+
+# Create your views here.
