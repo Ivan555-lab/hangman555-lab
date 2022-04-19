cd118e6d78681da4837885ab3b28886370fc433f
ivan555-lab <ivanchapay555@gmail.com>
2022-04-19 19:34:46 +0300

    '1948'

--- new.py
+++ new.py
@@ -1,21 +1,19 @@
+p = {
+    'c': 'th',
+    't': ['m', 'o', 'e'],
+}
+print("you ordered a " + p['c'] + "-c p" +
+      ' with ')
+for t in p['t']:
+    print("\t" + t)
 
-a_0 = {}
 
 
-#print("You just " + str(n_p) + " points!")
-a_0['z_p'] = 0
-a_0['y_p'] = 25
-a_0['x_p'] = 2
-#del a_0['y_p']
-#for key, value in a_0.items():
-#    print("\nKey: " + str(key))
-#    print("Value: " + str(value))
-#for n in a_0.keys():
-#   print(n.title())
-if 'z_p' not in a_0.keys():
-    print('We have to add')
-for n in a_0.values():
-    print(str(n) + ", thank you.")
+
+
+
+
+
 
 
 
