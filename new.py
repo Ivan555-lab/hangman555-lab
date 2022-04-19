c925ce992a3096ebf4f142c9717eb5a837b326c9
ivan555-lab <ivanchapay555@gmail.com>
2022-04-19 18:42:47 +0300

    '1947'

--- new.py
+++ new.py
@@ -1,12 +1,21 @@
 
-a_t = ['a', 'c']
-r_t = ['a', 'c']
+a_0 = {}
 
-for r in r_t:
-    if r in a_t:
-        print("y")
-    else:
-        print('s')
+
+#print("You just " + str(n_p) + " points!")
+a_0['z_p'] = 0
+a_0['y_p'] = 25
+a_0['x_p'] = 2
+#del a_0['y_p']
+#for key, value in a_0.items():
+#    print("\nKey: " + str(key))
+#    print("Value: " + str(value))
+#for n in a_0.keys():
+#   print(n.title())
+if 'z_p' not in a_0.keys():
+    print('We have to add')
+for n in a_0.values():
+    print(str(n) + ", thank you.")
 
 
 
