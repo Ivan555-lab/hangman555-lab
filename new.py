b7734e2e5b05e3789bad35908858dce907a5fc53
ivan555-lab <ivanchapay555@gmail.com>
2022-04-20 07:18:52 +0300

    '2041'

--- new.py
+++ new.py
@@ -1,22 +1,7 @@
-users = {
-    'a': {
-        'f':'a',
-        'l':'e',
-        'loc':'l',
-    },
-    'm': {
-        'f':'m',
-        'l':'c',
-        'loc': 'p'
-    },
-}
-
-for username, user_info in users.items():
-    print("\nUsername: " + username)
-    full_name = user_info['f'] + " " + user_info['l']
-    location = user_info['loc']
-    print("\tFull name: " + full_name.title())
-    print("\tLocation: " + location.title())
+prompt = "If "
+prompt += "\nWhat "
+m = input(prompt)
+print("Hello " + m + "!")
 
 
 
