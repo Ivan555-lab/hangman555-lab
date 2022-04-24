dafee8b6399a01ada5b4567875e28dee25acf59d
ivan555-lab <ivanchapay555@gmail.com>
2022-04-24 12:58:31 +0300

    '2442'

--- survey.py
+++ survey.py
@@ -1,1 +1,21 @@
+class AnonymousSurvey():
+    """Survey"""
 
+    def __init__(self, question):
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """Выводит вопрос."""
+
+        print(question)
+
+    def store_response(self, new_response):
+        """Save"""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """Out all res"""
+        print("Survey results:")
+        for response in responses:
+            print('- ' + response)
--- test_survey.py
+++ test_survey.py
@@ -1,1 +1,32 @@
+import unittest
+from survey import AnonymousSurvey
 
+class TestAnonymousSurvey(unittest.TestCase):
+    """Test classes"""
+
+    def setUp(self):
+        """setUp"""
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+        
+    def test_store_single_response(self):
+        """Check one response"""
+        qeustion = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(qeustion)
+        my_survey.store_response('English')
+
+        self.assertIn('English', my_survey.responses)
+
+    def test_store_three_repsonses(self):
+        """Checks 3 results"""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+
+unittest.main()
