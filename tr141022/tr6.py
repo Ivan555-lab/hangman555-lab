042c52f08adce3604deb24c800483d490cbedce3
ivan555-lab <vnchapay@gmail.com>
2022-10-14 10:38:25 +0300

    tr6_3

--- tr141022/tr6.py
+++ tr141022/tr6.py
@@ -4,3 +4,7 @@ def requires_auth(f):
     @wrap(f)
     def decrated(*args, **kwargs):
         auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            authenticate()
+        return f(*args, **kwargs)
+    return decorated
