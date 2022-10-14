1fc9b33760b6e7a6f9613f68871e727151add99e
ivan555-lab <vnchapay@gmail.com>
2022-10-14 10:07:55 +0300

    tr5_1

--- tr141022/tr4.py
+++ tr141022/tr4.py
@@ -8,12 +8,14 @@ def a_new_decorator(a_func):
         print("I am doing after")
 
     return wrapTheFunction
-
+@a_new_decorator
 def a_function_requiring_decoration():
     print("I am func that require the decoration")
 
+print(a_function_requiring_decoration.__name__)
 
 
-a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+
+# a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
 
 a_function_requiring_decoration()
